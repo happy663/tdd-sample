a9ce827c80e56e22293ada2571050e5c38c0a62d
happy663 <tatu2425@gmail.com>
2025-04-16 14:51:13 +0900

    refactor: インスタンスの生成処理の共通化

--- lib/integer_range_test.ts
+++ lib/integer_range_test.ts
@@ -1,11 +1,16 @@
 import { IntegerRange } from "./integer_range";
 
 describe("整数閉区間を示すクラス", () => {
+    let integerRange: IntegerRange;
+    beforeEach(() => {
+        integerRange = new IntegerRange();
+    });
+
     test("下端点と上端点（整数）を受け取って文字列を返す", () => {
-        expect(new IntegerRange().numbersToString(3, 7)).toBe("[3, 7]");
+        expect(integerRange.numbersToString(3, 7)).toBe("[3, 7]");
     });
 
     test("整数閉区間内の数値を入力したらtrueを返す", () => {
-        expect(new IntegerRange().isInsideRange(3, 7, 5)).toBe(true);
+        expect(integerRange.isInsideRange(3, 7, 5)).toBe(true);
     });
 });
